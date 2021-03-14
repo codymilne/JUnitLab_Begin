b23a40f568a15b972ff5b32415b16c1665439494
Cody <codymilne@csus.edu>
2021-03-13 20:07:05 -0800

    Another test commit

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -42,14 +42,42 @@ class GiftCardTest {
 		
 		issuingStore = 1337;
 		balance = 100.00;
-		String expected = ("Remaining Balance:   0.00");
+		String expected1 = ("Remaining Balance:   0.00");
+		String expected2 = ("Invalid Transaction");
+		String expected3 = ("Amount Due: 120.00");
 		
 		card = new GiftCard(issuingStore, balance);
 		
-		String result = card.deduct(balance);
+		String result1 = card.deduct(balance);
+		String result2 = card.deduct(-20.00);
+		String result3 = card.deduct(120.00);
 		
 		assertEquals("deduct_RemainingBalance()",
-					expected, result);
+					expected1, result1);
+		assertEquals("deduct_RemainingBalance()",
+				expected2, result2);
+		assertEquals("deduct_RemainingBalance()",
+				expected3, result3);
+		
+		
+	}
+	@Test
+	public void illegalBalance() {
+		
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+		
 	}
 	
+
+	@Test
+	public void constructor_IncorrectID_Low()  {
+		
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100);});
+	}
+	
+	@Test
+	public void constructor_IncorrectID_High()  {
+		
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(10000,100);});
+	}
 }
