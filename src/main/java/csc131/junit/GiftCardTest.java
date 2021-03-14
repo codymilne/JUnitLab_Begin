594e9fb9a5d6f1054c89adf14bdc6f7e0a27b952
Cody <codymilne@csus.edu>
2021-03-13 18:21:17 -0800

    "This is a commit message in Vim"

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,55 @@
+package csc131.junit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GiftCardTest {
+
+	@Test
+	public void getIssuingStore()
+	 {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore ()",
+					 issuingStore, card.getIssuingStore());
+	 }
+	@Test
+	public void getBalance() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getBalance ()",
+					 balance, card.getBalance(),0.001);
+	}
+	@Test
+	public void deduct_RemainingBalance() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		String expected = ("Remaining Balance:   0.00");
+		
+		card = new GiftCard(issuingStore, balance);
+		
+		String result = card.deduct(balance);
+		
+		assertEquals("deduct_RemainingBalance()",
+					expected, result);
+	}
+	
+}
